fd43c75fe08708fb3b07edfa060ef95fa434e442
Angsining <angsining@stu2020.jnu.edu.cn>
2022-09-07 10:48:46 +0800

    Initial commit

---
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jnu.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle("我的第一个应用");
+    }
+}
